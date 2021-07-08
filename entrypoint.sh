d448034a02351714d7aaa921a322823f4a0a91c7
Shrinidhi S Rao <shrinidhi111@live.com>
2021-07-08 19:23:08 +0530

    multi binary attempt 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,33 @@ echo "${WINDOWS_CERT}" > /certs/bundle.crt
 echo "${WINDOWS_KEY}" > /certs/codesign.key
 
 echo "--> signing binary"
-/osslsigncode/osslsigncode-1.7.1/osslsigncode sign -certs /certs/bundle.crt -key /certs/codesign.key -h sha256 -n ${NAME} -i ${DOMAIN} -t "http://timestamp.verisign.com/scripts/timstamp.dll" -in ${BINARY} -out /signedbinary
 
-echo "--> overwriting existing binary with signed binary"
-cp /signedbinary ${BINARY}
+
+## declare an array variable
+declare -a array=($@)
+
+
+
+# get length of an array
+arraylength=${#array[@]}
+
+# use for loop to read all values and indexes
+for (( i=0; i<${arraylength}; i++ ));
+do
+  echo "index: $i, value: ${array[$i]}"
+
+  
+  /osslsigncode/osslsigncode-1.7.1/osslsigncode sign -certs /certs/bundle.crt -key /certs/codesign.key -h sha256 -n ${NAME} -i ${DOMAIN} -t "http://timestamp.verisign.com/scripts/timstamp.dll" -in ${array[$i]} -out /signedbinary
+
+  echo "--> overwriting existing binary with signed binary"
+  cp /signedbinary ${array[$i]}
+
+
+done
+
+
+
+# /osslsigncode/osslsigncode-1.7.1/osslsigncode sign -certs /certs/bundle.crt -key /certs/codesign.key -h sha256 -n ${NAME} -i ${DOMAIN} -t "http://timestamp.verisign.com/scripts/timstamp.dll" -in ${BINARY} -out /signedbinary
+
+# echo "--> overwriting existing binary with signed binary"
+# cp /signedbinary ${BINARY}
