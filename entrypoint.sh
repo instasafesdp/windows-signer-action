faf3c029838ca57ebaea4887816ac88f65ba0678
Shrinidhi S Rao <shrinidhi111@live.com>
2021-07-09 02:51:34 +0530

    using p12 files

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,10 @@ echo "--> outputting cert and key to files"
 mkdir /certs
 # The quotations around the cert/key vars are very import to handle line breaks
 echo "${WINDOWS_CERT}" | base64 -d > /certs/bundle.crt
-echo "${WINDOWS_KEY}" | base64 -d > /certs/codesign.key
+echo "${WINDOWS_PASS}" > /certs/codesign.key
 
-echo /certs/bundle.crt
-echo /certs/codesign.key
+cat /certs/bundle.crt
+cat /certs/codesign.key
 
 echo "--> signing binary"
 
@@ -32,14 +32,17 @@ for (( i=0; i<${arraylength}; i++ ));
 do
   echo "index: $i, value: ${array[$i]}"
 
+
+  #osslsigncode sign -pkcs12 <pfx-file> -pass <pfx-password> -n "Your Application" -i http://www.yourwebsite.com/ -in yourapp.exe -out yourapp-signed.exe
   
-  /osslsigncode/osslsigncode-1.7.1/osslsigncode sign -certs /certs/bundle.crt -key /certs/codesign.key -h sha256 -n ${NAME} -i ${DOMAIN} -t "http://timestamp.verisign.com/scripts/timstamp.dll" -in ${array[$i]} -out /signedbinary
+  /osslsigncode/osslsigncode-1.7.1/osslsigncode sign --pkcs12 /certs/bundle.crt -pass $WINDOWS_PASS -h sha256 -n ${NAME} -i ${DOMAIN} -t "http://timestamp.verisign.com/scripts/timstamp.dll" -in ${array[$i]} -out /signedbinary
 
   echo "--> overwriting existing binary with signed binary"
   cp /signedbinary ${array[$i]}
 
 
 done
+
 
 
 
